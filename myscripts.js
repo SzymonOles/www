d15f892829f920be4f356163357fabe4f2f45675
SzymonOles <szymon.oles.2k@gmail.com>
2022-12-15 09:18:46 +0100

    final v1

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+  // darkmode
   var dark = false;
   document.getElementById("nightmode").addEventListener("click", togglen);
 
@@ -16,5 +17,35 @@ window.addEventListener("load", function () {
 
   function togglen() {
     dark ? day() : night();
+  }
+// timer
+  function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('clock').textContent =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+  }
+  startTime()
+
+  // dropdown
+  document.getElementById("dropdown").addEventListener("click", togglemenu);
+
+  function togglemenu(){
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
   }
 });
